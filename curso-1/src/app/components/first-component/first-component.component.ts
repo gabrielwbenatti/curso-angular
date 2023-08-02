0b9084469a0389558a42e2778af8a3a6b1d68d50
Gabriel Wisky Benatti <70673950+gabrielwbenatti@users.noreply.github.com>
2023-08-02 12:00:02 -0300

    aula 5

--- curso-1/src/app/components/first-component/first-component.component.ts
+++ curso-1/src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
- 
+import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit {
+  name: string = 'Gabriel';
+  age: number = 10;
+  job = 'Developer';
+  hobbies = ['correr', 'jogar', 'estudar'];
+  car = {
+    name: 'onix',
+    year: 2020,
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
