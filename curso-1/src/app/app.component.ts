7fc98649faa0d1697420e6c25e2baf904cc6fb1f
Gabriel Wisky Benatti <70673950+gabrielwbenatti@users.noreply.github.com>
2023-08-03 11:57:30 -0300

    aula 7

--- curso-1/src/app/app.component.ts
+++ curso-1/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent { 
+export class AppComponent {
+  userName = 'Joaquim';
+  userData = { email: 'joaquim@email.com', role: 'Admin' };
+
   title = 'curso-1';
 }
