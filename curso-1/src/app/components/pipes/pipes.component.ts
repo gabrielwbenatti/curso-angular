2241c0179059915476117b01035d3859fc917f2c
Gabriel Wisky Benatti <70673950+gabrielwbenatti@users.noreply.github.com>
2023-08-09 11:29:46 -0300

    aula 14 - pipe operator

---
+++ curso-1/src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrls: ['./pipes.component.css'],
+})
+export class PipesComponent {
+  someText = 'Algum texto aqui';
+  today = new Date();
+}
