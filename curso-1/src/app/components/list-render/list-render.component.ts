b6e7f946ead296a40fafe5f693fee4aeb7f3097e
Gabriel Wisky Benatti <70673950+gabrielwbenatti@users.noreply.github.com>
2023-08-09 11:56:35 -0300

    aula 16 - services

--- curso-1/src/app/components/list-render/list-render.component.ts
+++ curso-1/src/app/components/list-render/list-render.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
+
+import { ListService } from 'src/app/serivices/list.service';
 
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css'],
 })
-export class ListRenderComponent {
+export class ListRenderComponent implements OnInit {
   animals: Animal[] = [
+    { name: 'Turca', type: 'Dog', age: 4 },
     { name: 'Turca', type: 'Dog', age: 4 },
     { name: 'Tom', type: 'Cat', age: 10 },
     { name: 'Frida', type: 'Dog', age: 6 },
@@ -19,5 +22,14 @@ export class ListRenderComponent {
 
   showAge(animal: Animal): void {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
+  }
+
+  constructor(private listService: ListService) {}
+
+  ngOnInit(): void {}
+
+  removeAnimal(animal: Animal): void {
+    console.log('Removendo animal...');
+    this.animals = this.listService.remove(this.animals, animal);
   }
 }
